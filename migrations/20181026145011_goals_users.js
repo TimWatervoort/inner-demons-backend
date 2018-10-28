fdea0534222431d9f77b880a15d4bfb723732b57
Tim Remington <timremington@Tims-MacBook-Pro.local>
2018-10-28 12:18:37 -0600

    Fixed updates

--- migrations/20181026145011_goals_users.js
+++ migrations/20181026145011_goals_users.js
@@ -1,10 +1,12 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('goals_users', table => {
+
     table.increments();
-    table.integer('goal_id');
-    table.foreign('goal_id').references('goals.id');
-    table.integer('user_id');
-    table.foreign('user_id').references('users.id');
+    table.integer('goal_id').notNullable();
+    table.foreign('goal_id').references('goals.id').onDelete('CASCADE');
+
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('users.id').onDelete('CASCADE');
   });
 };
 
--- migrations/20181026145027_goals_tasks.js
+++ migrations/20181026145027_goals_tasks.js
@@ -1,11 +1,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('goals_tasks', table => {
-    table.increments('id')
-    table.integer('goal_id')
-    table.foreign('goal_id').references('goals.id')
-    table.integer('task_id')
-    table.foreign('task_id').references('tasks.id')
-    table.timestamps(true, true)
+
+    table.increments();
+    table.integer('goal_id').notNullable();
+    table.foreign('goal_id').references('goals.id').onDelete('CASCADE');
+
+    table.integer('task_id').notNullable();
+    table.foreign('task_id').references('tasks.id').onDelete('CASCADE');
+    //table.timestamps(true, true);
   })
 }
 
