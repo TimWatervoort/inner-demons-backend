7eb5aab7b4617e3d97fffecbe4637921e0101c9a
Tim Remington <timremington@Tims-MacBook-Pro.local>
2018-10-28 14:07:07 -0600

    Modifications to join tables

--- routes/goals_users.js
+++ routes/goals_users.js
@@ -1,6 +1,60 @@
 const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
+const Joi = require('joi')
+
+const validateUserID = (req, res, next) => {
+  knex('goals_users')
+  .where('id', req.params.id)
+  .then(([data]) => {
+    if (!data) {
+      return res.status(400).json({ error: { message: `User ID ${req.params.id} not found` } })
+    }
+    next()
+  })
+}
+
+const validatePostBody = (req, res, next) => {
+  const postSchema = Joi.object().keys({
+    goal_id: Joi.number().integer().required(),
+    user_id: Joi.number().integer().required()
+  })
+
+  const { error } = Joi.validate(req.body, postSchema)
+
+  if (error) {
+    return res.status(400).json({ "POST Schema Error": { message: error.details[0].message } })
+  }
+  next()
+}
+
+const buildPatchReq = (req, res, next) => {
+  const patchSchema = Joi.object().keys({
+    goal_id: Joi.number().integer(),
+    user_id: Joi.number().integer()
+  })
+
+  const { error } = Joi.validate(req.body, patchSchema)
+  if (error) {
+    return res.status(400).json({ "PATCH Schema Error": { message: error.details[0].message } })
+  }
+
+  const allowedPatchKeys = ['goal_id', 'user_id']
+  // Constructs the patch request object
+  let patchReq = {}
+  allowedPatchKeys.forEach(key => {
+    if (req.body.hasOwnProperty(key)) { patchReq[key] = req.body[key] }
+  })
+
+  // If the patch request is empty or has invalid key names, return an error
+  if (Object.keys(patchReq).length === 0) {
+    return res.status(400).json({ error: { message: `Empty or invalid patch request` } })
+  }
+
+  // Stores the patch request object into request
+  req.patchReq = patchReq
+  next()
+}
 
 /* GET all goals record */
 router.get('/', (req, res, next) => {
@@ -10,7 +64,7 @@ router.get('/', (req, res, next) => {
 })
 
 /* GET single goals record */
-router.get('/:id', (req, res, next) => {
+router.get('/:id', validateUserID, (req, res, next) => {
   knex('goals_users')
   .where('id', req.params.id)
   .then(([data]) => res.status(200).json(data))
@@ -18,7 +72,7 @@ router.get('/:id', (req, res, next) => {
 })
 
 /* POST new goals record */
-router.post('/', (req, res, next) => {
+router.post('/', validatePostBody, (req, res, next) => {
   const { goal_id, user_id } = req.body
 
   knex('goals_users')
@@ -29,7 +83,7 @@ router.post('/', (req, res, next) => {
 })
 
 /* PATCH specified goals record */
-router.patch('/:id', (req, res, next) => {
+router.patch('/:id', validateUserID, buildPatchReq, (req, res, next) => {
   const { patchReq } = req
 
   knex('goals_users')
@@ -44,7 +98,7 @@ router.patch('/:id', (req, res, next) => {
 })
 
 /* DELETE specified goals record */
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', validateUserID, (req, res, next) => {
   knex('goals_users')
   .where('id', req.params.id)
   .first()
