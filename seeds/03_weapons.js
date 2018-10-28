271281d8e5c262b0183bcf0222635ddb96022f91
Tim Remington <timremington@Tims-MacBook-Pro.local>
2018-10-28 09:30:40 -0600

    Added chaos

--- seeds/03_weapons.js
+++ seeds/03_weapons.js
@@ -7,19 +7,25 @@ exports.seed = function(knex, Promise) {
           id: 1,
           name: 'Disappointing Glance',
           description: 'The look that hurts the most.',
-          attack: 1
+          attack: 2,
+          chaos: 1,
+          image: ''
         },
         {
           id: 2,
           name: 'Shoe',
           description: 'Just a shoe',
-          attack: 1
+          attack: 2,
+          chaos: 1,
+          image: ''
         },
         {
           id: 3,
           name: 'Fist',
           description: 'Your right hand curled into a ball.  Dude, find a new weapon.',
-          attack: 1
+          attack: 2,
+          chaos: 1,
+          image: ''
         }
       ]);
     })
