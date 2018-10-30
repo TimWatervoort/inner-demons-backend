55a51a94ada9dd4b5387dce14675f97539bfe21e
Tim Watervoort <twwatervoort@gmail.com>
2018-10-30 14:47:22 -0600

    added fail screen

--- public/scripts/bossbattle.js
+++ public/scripts/bossbattle.js
@@ -103,6 +103,7 @@ function dukeItOut(ally, weapon, monster, user) {
   console.log("User Health: ", userHP)
   if (userHP <= 0) {
     setHere.innerHTML += `<br>You were killed by ${monster.name}!`
+    setTimeout(() => killScreen(monster), 3000)
     console.log("You were killed.")
     return "You were killed."
   }
@@ -121,4 +122,9 @@ function dukeItOut(ally, weapon, monster, user) {
 function victoryScreen(monster) {
   setHere.innerHTML = '';
   setHere.innerHTML = `<h3 class='text-center text-white'>Congratulations!</h3><hr><img class = 'weaponImg' src=${monster.image} style='margin:auto;'><br><h5 class = 'text-center text-white'>${monster.name} is now available as an ally.</h5>`
+}
+
+function killScreen(monster) {
+  setHere.innerHTML = ''
+  setHere.innerHTML = `<h3 class='text-center text-white'>You have been slain!</h3><hr><img class = 'weaponImg' src=${monster.image} style='margin:auto;'><br><h5 class = 'text-center text-white'>${monster.name} was too great a foe.</h5>`
 }
